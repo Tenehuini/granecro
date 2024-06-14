ef955c794d95f5066461ee03aa8bc2079185b881
tomas.hampejs <caerulus@gmail.com>
2024-06-14 16:32:48 +0200

    init

---
+++ granecro.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# server port 6201
+
+st.write("Semestr 1")
+st.image("images/back.png", width=100)
+
+st.write("Semestr 2")
+st.image("images/back.png", width=100)
+
+# which components
